229a0c844b7b25cda6b2d8b23f6587ec51571e42
Stilla <sunnatilla95@gmail.com>
2026-02-24 11:29:54 +0500

    yengi project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Akbarjonka</div>
+  )
+}
+
+export default Header
---
+++ src/components/Section.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <div>Abdummanop</div>
+  )
+}
+
+export default Section
